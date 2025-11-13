c5e34c16f7470b42250971a4a28192d03829e671
JVHBO <zoboomafoohbo@gmail.com>
2025-11-13 03:37:40 -0300

    Add barrel export for NFT utilities

---
+++ lib/nft/index.ts
@@ -0,0 +1,19 @@
+/**
+ * NFT Utilities Barrel Export
+ *
+ * Centralized exports for NFT-related utilities
+ */
+
+// Attribute parsing and calculations
+export {
+  findAttr,
+  isUnrevealed,
+  calcPower,
+  normalizeUrl,
+} from "./attributes";
+
+// NFT fetching and image handling
+export {
+  getImage,
+  fetchNFTs,
+} from "./fetcher";
